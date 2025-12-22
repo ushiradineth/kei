2d3d1abaf83666d77b55aa1b22dfc3a84078459b
Ushira Dineth <ushiradineth@gmail.com>
2025-12-22 20:24:46 +0530

    feat: add nix support
    Signed-off-by: Ushira Dineth <ushiradineth@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import (inputs.nixpkgs) {inherit system;};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go_1_25
+
+            pkgs.gopls
+            pkgs.gofumpt
+            pkgs.goimports-reviser
+            pkgs.golines
+            pkgs.golangci-lint
+            pkgs.air
+            pkgs.govulncheck
+            pkgs.gow
+
+            pkgs.vimPlugins.nvim-treesitter-parsers.go
+            pkgs.vimPlugins.nvim-treesitter-parsers.gomod
+          ];
+        };
+      }
+    );
+}
